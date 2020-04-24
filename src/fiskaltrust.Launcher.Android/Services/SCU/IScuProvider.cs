97d3ab7d9d126107cd442281f7eae154dc3b19d1
Tom Schmiedlechner <tschmiedlechner@outlook.com>
2020-04-24 13:32:13 +0200

    Added fiskaly SCU package

---
+++ src/fiskaltrust.Launcher.Android/Services/SCU/IScuProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using fiskaltrust.ifPOS.v1.de;
+using fiskaltrust.Middleware.SCU.DE.Fiskaly;
+
+namespace fiskaltrust.Launcher.Android.Services.SCU
+{
+    interface IScuProvider
+    {
+        public IDESSCD CreateScu(Dictionary<string, object> scuConfiguration)
+        {
+            return new FiskalySCU(scuConfiguration);
+        }
+    }
+}
