16774a5410ab63d2bd1fe445707e327d8f9e6374
Tom Schmiedlechner <tschmiedlechner@outlook.com>
2020-11-24 19:40:19 +0100

    Add missing endpoints

--- src/fiskaltrust.AndroidLauncher.Http/Controllers/POSController.cs
+++ src/fiskaltrust.AndroidLauncher.Http/Controllers/POSController.cs
@@ -15,9 +15,13 @@ namespace fiskaltrust.AndroidLauncher.Http.Controllers
         }
 
         [HttpPost("v0/echo")]
+        [HttpPost("json/v0/echo")]
+        [HttpPost("xml/v0/echo")]
         public ActionResult<string> Echo([FromBody] string request) => _pos.Echo(request);
 
         [HttpPost("v1/echo")]
+        [HttpPost("json/v1/echo")]
+        [HttpPost("xml/v1/echo")]
         [HttpPost("echo")]
         public async Task<ActionResult<EchoResponse>> EchoAsync([FromBody] EchoRequest request) => await _pos.EchoAsync(request);
 
@@ -25,10 +29,14 @@ namespace fiskaltrust.AndroidLauncher.Http.Controllers
         public ActionResult<ifPOS.v0.ReceiptResponse> Sign([FromBody] ifPOS.v0.ReceiptRequest request) => _pos.Sign(request);
 
         [HttpPost("v1/sign")]
+        [HttpPost("json/v1/sign")]
+        [HttpPost("xml/v1/sign")]
         [HttpPost("sign")]
         public async Task<ActionResult<ReceiptResponse>> SignAsync([FromBody] ReceiptRequest request) => await _pos.SignAsync(request);
 
         [HttpPost("v0/journal")]
+        [HttpPost("json/v0/journal")]
+        [HttpPost("xml/v0/journal")]
         [HttpPost("journal")]
         public ActionResult<Stream> Journal([FromQuery] long type, [FromQuery] long from, [FromQuery] long to) => _pos.Journal(type, from, to);
     }
