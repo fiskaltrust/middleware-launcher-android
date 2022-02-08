4adccd4e68a3a160658996fe45ade02e1767cf28
forsthug <85173816+forsthug@users.noreply.github.com>
2022-02-08 13:44:51 +0100

    activate button  in notification

--- src/fiskaltrust.AndroidLauncher.Http/MiddlewareLauncherHttpService.cs
+++ src/fiskaltrust.AndroidLauncher.Http/MiddlewareLauncherHttpService.cs
@@ -1,7 +1,12 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.OS;
 using fiskaltrust.AndroidLauncher.Common;
 using fiskaltrust.AndroidLauncher.Common.AndroidService;
+using fiskaltrust.AndroidLauncher.Common.Constants;
 using fiskaltrust.AndroidLauncher.Common.Helpers;
+using fiskaltrust.AndroidLauncher.Common.Services;
+using fiskaltrust.AndroidLauncher.Http.Broadcasting;
 using fiskaltrust.AndroidLauncher.Http.Helpers;
 using fiskaltrust.AndroidLauncher.Http.Hosting;
 
@@ -13,5 +18,13 @@ namespace fiskaltrust.AndroidLauncher.Http
         public override IHostFactory GetHostFactory() => new HttpHostFactory();
 
         public override IUrlResolver GetUrlResolver() => new RestUrlResolver();
+
+        public override IBinder OnBind(Intent intent)
+        {
+            var binder = new POSProviderBinder(this);
+            var stopBroadcastReceiver = new StopLauncherBroadcastReceiver();
+            RegisterReceiver(stopBroadcastReceiver, new IntentFilter(BroadcastConstants.StopBroadcastName));
+            return binder;
+        }
     }
 }
