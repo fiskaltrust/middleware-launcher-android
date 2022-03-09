d860abf5df2c463933e296d596cbb1c248cf827d
Paul Volavsek <paul.volavsek@gmail.com>
2022-03-09 14:29:52 +0100

    fix

--- azure-pipelines/scripts/create-cashbox.ps1
+++ azure-pipelines/scripts/create-cashbox.ps1
@@ -12,12 +12,7 @@ $template = [string](Get-Content $TemplatePath -Raw)
 $uri = "https://helipad-sandbox.fiskaltrust.cloud/api/configuration?description=delete-launcher-smoketests-$($env:BUILD_BUILDID)&fyapikey=$([uri]::EscapeDataString($FiskalyApiKey))&fyapisecret=$([uri]::EscapeDataString($FiskalyApiSecret))&fytssid=$([uri]::EscapeDataString($TssId))&fyadminpin=$([uri]::EscapeDataString($TssAdminPin))"
 $headers = @{ accountid = $AccountId; accesstoken = $AccountAccessToken }
 
-echo "template:"
-echo $TemplatePath
-echo $template
-echo $($template | ConvertTo-Json)
-
-$response = Invoke-WebRequest -uri $uri -Headers $headers -Method POST -ContentType "application/json; charset=utf-8" -Body $($template | ConvertTo-Json).value | ConvertFrom-Json
+$response = Invoke-WebRequest -uri $uri -Headers $headers -Method POST -ContentType "application/json; charset=utf-8" -Body $($template | ConvertTo-Json) | ConvertFrom-Json
 
 echo "##vso[task.setvariable variable=cashboxId;isOutput=true]$($response.cashboxid)"
 echo "##vso[task.setvariable variable=accessToken;isOutput=true]$($response.accesstoken)"
