f01d9802e4257b52959de35640095e8aaa4426ad
Paul Volavsek <paul.volavsek@gmail.com>
2022-03-09 13:22:35 +0100

    add admin pin

--- azure-pipelines/scripts/create-cashbox.ps1
+++ azure-pipelines/scripts/create-cashbox.ps1
@@ -4,18 +4,16 @@ param(
   [string]$TemplatePath,
   [string]$FiskalyApiKey,
   [string]$FiskalyApiSecret,
+  [string]$TssAdminPin,
   [string]$TssId)
 
 $template = Get-Content $TemplatePath -Raw
-$templateJson = $template | ConvertFrom-Json
 
-$uri = "https://helipad-sandbox.fiskaltrust.cloud/api/configuration?description=delete-launcher-smoketests-$($env:BUILD_BUILDID)&fyapikey=$([uri]::EscapeDataString($FiskalyApiKey))&fyapisecret=$([uri]::EscapeDataString($FiskalyApiSecret))&fytssid=$([uri]::EscapeDataString($TssId))"
+$uri = "https://helipad-sandbox.fiskaltrust.cloud/api/configuration?description=delete-launcher-smoketests-$($env:BUILD_BUILDID)&fyapikey=$([uri]::EscapeDataString($FiskalyApiKey))&fyapisecret=$([uri]::EscapeDataString($FiskalyApiSecret))&fytssid=$([uri]::EscapeDataString($TssId))&fyadminpin=$([uri]::EscapeDataString($TssAdminPin))"
 $headers = @{ accountid = $AccountId; accesstoken = $AccountAccessToken }
 
 $response = Invoke-WebRequest -uri $uri -Headers $headers -Method POST -ContentType "application/json; charset=utf-8" -Body $($template | ConvertTo-Json) | ConvertFrom-Json
 
-echo $($response | ConvertTo-Json)
-
 echo "##vso[task.setvariable variable=cashboxId;isOutput=true]$($response.cashboxid)"
 echo "##vso[task.setvariable variable=accessToken;isOutput=true]$($response.accesstoken)"
-echo "##vso[task.setvariable variable=url;isOutput=true]$($templateJson.ftQueues[0].Url[0])"
+echo "##vso[task.setvariable variable=url;isOutput=true]$($($response.configuration | ConvertFrom-Json).ftQueues[0].Url[0])"
