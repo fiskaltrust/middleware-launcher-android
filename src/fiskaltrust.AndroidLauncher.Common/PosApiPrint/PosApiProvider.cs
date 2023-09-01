c1ab5d6bfcce0657f05255b93c1e1eae3aafccb8
Stefan Kert <stefan.kert@sbg.at>
2023-09-01 13:50:01 +0200

    Fixed build issue

--- src/fiskaltrust.AndroidLauncher.Common/PosApiPrint/PosApiProvider.cs
+++ src/fiskaltrust.AndroidLauncher.Common/PosApiPrint/PosApiProvider.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using fiskaltrust.AndroidLauncher.Common.PosApiPrint.Models;
 using fiskaltrust.ifPOS.v1;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
