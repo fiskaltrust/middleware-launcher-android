2791c60d594fee85ba3e769cd8e177fe2642f8ee
Leila <67684543+LeilaZarban@users.noreply.github.com>
2026-06-11 13:10:50 +0200

    remove localpossystem folder

--- src/fiskaltrust.Api.PosSystemLocal/Storage/Interfaces/IAsyncStorageBootstrapper.cs
+++
@@ -1,9 +0,0 @@
-using fiskaltrust.storage.serialization.V0;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace fiskaltrust.Middleware.Storage.AzureTableStorage;
-
-public interface IAsyncStorageBootstrapper
-{
-    public Task ConfigureStorageServicesAsync(PackageConfiguration configuration, IServiceCollection services);
-}
