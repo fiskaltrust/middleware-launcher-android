58744ea9cb4f7344ed782ca6dc562ebb761f0940
Pawel van de Sanden <karczewskipawel.j@gmail.com>
2026-08-25 11:58:13 +0200

    Refactor: Remove enableCloseButton parameter and related logic from MiddlewareLauncherService and PosSystemApiRequestHandler

--- src/fiskaltrust.AndroidLauncher/Constants/BroadcastConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace fiskaltrust.AndroidLauncher.Constants
-{
-    public class BroadcastConstants
-    {
-        public const string StopBroadcastAction = "eu.fiskaltrust.androidlauncher.Stop";
-    }
-}
